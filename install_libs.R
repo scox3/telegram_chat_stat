e53a3fb39184757e2f967de5c3d08615e386f435
Vladislav Borkus <41326611+scox3@users.noreply.github.com>
2022-11-23 15:30:08 +0300

    Add files via upload

--- install_libs.R
+++ install_libs.R
@@ -1,19 +1,18 @@
-
-
-
-#Parse init_libs file for libraries names
-
 con <- file("init_libs.R", open="r")
 s <- readLines(con)
 close(con)
 
 s <- grep("library\\((.+?)\\)", s, value=TRUE)
 s <- sapply(s, function(x){ substr(x,9, nchar(x)-1) } )
-s <- paste0('install.packages("',s, '")')
 
 for( s1 in s ){
   print(s1)
+  
+  if( !require(s1, character.only = TRUE)) {
   #execute installation command 
-  eval(parse(text=s1))
-  Sys.sleep(5)
+    install.packages(s1)
+    Sys.sleep(5)
+  } else {
+    print( paste0("Already installed: ", s1))
+  }
 }
